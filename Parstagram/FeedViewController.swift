a56222ab3f634fe452386be8e2f3dd2deb8c95e9
Katie <kitkatie117@gmail.com>
2021-10-23 00:41:16 -0400

    Add logout

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -84,4 +84,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     */
 
+    @IBAction func onLogout(_ sender: UIBarButtonItem) {
+        
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
 }
